66ec838f5b40f268b6ec38c2c1b57d7cb7da3140
vikkies <125499337+vikkies@users.noreply.github.com>
2023-06-05 19:16:29 +0300

    Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,20 @@
 import pygame
 from sys import exit
 from Classes import Shooter
-
-money = 0
+import Images
 
 display = pygame.display.set_mode((1200,800))
-map = pygame.image.load(r"C:\Users\emmir\Downloads\TD_Map.jpg").convert()
+map = Images.map.convert()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        elif Shooter.deploy():
+
+        if Shooter.deploy():
             Shooter.deploy()
+
 
     display.blit(map,(0,0))
     pygame.draw.rect(display,"Blue",Shooter.shooter_rect)
